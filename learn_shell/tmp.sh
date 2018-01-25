bb6ca4436133aa2d04a34a8e5537f8906711d1d3
wangbo <wangbo2008@vip.qq.com>
2018-01-25 08:07:57 +0000

    v1

--- learn_shell/tmp.sh
+++ learn_shell/tmp.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-H2G2=42
-str='echo "$FOO$H2G2 does not contain: bar"'
-str2="echo $H2G2"
-bash -c "${str}"
-bash -c "${str2}"
+export "H2G2=42"
+# echo $H2G2
+#eval "H2G2=42"
+# str='echo "$FOO$H2G2 does not contain: bar"'
+# str2="
+
+# let H2G2++
+# echo "121212 $H2G2"
+# sleep 10
+# echo "over"
+# bash -c "${str}"
+# bash -c "${str2}"
+# line_n=`grep -c "" $1`
+# echo $line_n
+
+# procfile=$1
+# line_number=0
+# line_n=`grep -c "" ${procfile}`
+# while read line; do
+# 	let line_number++
+# done < "${procfile}"
+# if [[ ${line_number} -ne ${line_n} ]]; then
+# 	echo "666666666666"
+# fi
+
+#bash -c "ls" > >(cat) 2>&1
+
+# bash -c "ls" > >(
+#     while read line; do
+# 		echo "file is $line"
+# 	done)
+
+cat <(ls)
+
